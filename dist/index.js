db38e20031612979caeefa32bd979fd0ed922143
DanishNazakat <danishnazakat636@gmail.com>
2026-04-03 11:37:04 +0500

    add script

--- dist/index.js
+++ dist/index.js
@@ -2,5 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 let message = 30;
 console.log(message);
-// let name = 30;
-// console.log(name);
+let name = 30;
+console.log(name);
