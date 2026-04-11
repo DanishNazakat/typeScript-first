db90959aadf5882ee768eb7718b73d03f221abe2
DanishNazakat <danishnazakat636@gmail.com>
2026-04-11 14:22:01 +0500

    typeScript

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,5 @@
 "use strict";
 // let message: string|number = "hello TypeScript";
 Object.defineProperty(exports, "__esModule", { value: true });
-let campusDAta = {
-    staffId: 10001,
-    staffName: "abid",
-    StudentId: 23231,
-    // StudentName : "Danish"
-};
+
 console.log(campusDAta);
